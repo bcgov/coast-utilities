b096444a48820ce86ec37bb48bf31415984807fc
KKAYFISH <kyle.kayfish@gmail.com>
2022-09-20 10:43:12 -0700

    Core31Update - Project builds with 4 warning of unused fields (these are unchanged from update)

--- CASInterfaceService/CASInterfaceService/Startup.cs
+++ CASInterfaceService/CASInterfaceService/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CASInterfaceService
 {
@@ -29,14 +30,19 @@ namespace CASInterfaceService
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(opts =>
+            {
+                opts.EnableEndpointRouting = false;
+            }
+            ).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
